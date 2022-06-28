a925193326128b84a9d960d7905db3193d656997
GUTTAPPA SAJJAN <45628395+Guttappa1238@users.noreply.github.com>
2022-06-28 16:09:17 +0530

    updated
    black formated

--- yolov6/models/yolo.py
+++ yolov6/models/yolo.py
@@ -25,7 +25,7 @@ class Detect(nn.Module):
         self.grid = [torch.zeros(1)] * self.nl
         self.prior_prob = 1e-2
         self.inplace = inplace
-        
+
         # Init decouple head
         self.cls_convs = nn.ModuleList()
         self.reg_convs = nn.ModuleList()
@@ -44,11 +44,11 @@ class Detect(nn.Module):
             b = conv.bias.view(self.na, -1)
             b.data.fill_(-math.log((1 - self.prior_prob) / self.prior_prob))
             conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-               
+
     def forward(self, x):
         z = []
         for i in range(self.nl):
-           
+
             x[i] = self.stems[i](x[i])
             cls_x = x[i]
             reg_x = x[i]
@@ -59,36 +59,53 @@ class Detect(nn.Module):
             reg_feat = self.reg_convs[i](reg_x)
             reg_output = self.reg_preds[i](reg_feat)
             obj_output = self.obj_preds[i](reg_feat)
-            
+
             if self.training:
                 x[i] = torch.cat([reg_output, obj_output, cls_output], 1)
                 bs, _, ny, nx = x[i].shape
-                x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+                x[i] = (
+                    x[i]
+                    .view(bs, self.na, self.no, ny, nx)
+                    .permute(0, 1, 3, 4, 2)
+                    .contiguous()
+                )
 
             else:
-                y = torch.cat([reg_output, obj_output.sigmoid(), cls_output.sigmoid()], 1)
+                y = torch.cat(
+                    [reg_output, obj_output.sigmoid(), cls_output.sigmoid()], 1
+                )
                 bs, _, ny, nx = y.shape
-                y = y.view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+                y = (
+                    y.view(bs, self.na, self.no, ny, nx)
+                    .permute(0, 1, 3, 4, 2)
+                    .contiguous()
+                )
 
                 if self.grid[i].shape[2:4] != y.shape[2:4]:
                     d = self.stride.device
-                    yv, xv = torch.meshgrid([torch.arange(ny).to(d), torch.arange(nx).to(d)])
-                    self.grid[i] = torch.stack((xv, yv), 2).view(1, self.na, ny, nx, 2).float()
+                    yv, xv = torch.meshgrid(
+                        [torch.arange(ny).to(d), torch.arange(nx).to(d)]
+                    )
+                    self.grid[i] = (
+                        torch.stack((xv, yv), 2).view(1, self.na, ny, nx, 2).float()
+                    )
 
                 if self.inplace:
                     y[..., 0:2] = (y[..., 0:2] + self.grid[i]) * self.stride[i]  # xy
-                    y[..., 2:4] = torch.exp(y[..., 2:4]) * self.stride[i] # wh
-                else:  
+                    y[..., 2:4] = torch.exp(y[..., 2:4]) * self.stride[i]  # wh
+                else:
                     xy = (y[..., 0:2] + self.grid[i]) * self.stride[i]  # xy
                     wh = torch.exp(y[..., 2:4]) * self.stride[i]  # wh
                     y = torch.cat((xy, wh, y[..., 4:]), -1)
-                z.append(y.view(bs, -1, self.no))    
+                z.append(y.view(bs, -1, self.no))
 
         return x if self.training else torch.cat(z, 1)
 
 
 class Model(nn.Module):
-    def __init__(self, config, ch=3, nc=None, anchors=None):  # model, input channels, number of classes
+    def __init__(
+        self, config, ch=3, nc=None, anchors=None
+    ):  # model, input channels, number of classes
         super().__init__()
         # Build network
         model, self.backbone, self.neck = build_network(config, ch, nc)
@@ -103,13 +120,13 @@ class Model(nn.Module):
         m.i = bi
         m.f = oi_head
         for i in range(nl):
-            idx = i*6
+            idx = i * 6
             m.stems.append(model[idx])
-            m.cls_convs.append(model[idx+1])
-            m.reg_convs.append(model[idx+2])
-            m.cls_preds.append(model[idx+3])
-            m.reg_preds.append(model[idx+4])
-            m.obj_preds.append(model[idx+5])
+            m.cls_convs.append(model[idx + 1])
+            m.reg_convs.append(model[idx + 2])
+            m.cls_preds.append(model[idx + 3])
+            m.reg_preds.append(model[idx + 4])
+            m.obj_preds.append(model[idx + 5])
         self.detect = m
         self.detect.initialize_biases()
 
@@ -142,19 +159,20 @@ def build_network(config, ic, nc):
     num_repeat_neck = config.model.neck.num_repeats
     channels_list_neck = config.model.neck.out_channels
     num_anchors = config.model.head.anchors
-    num_repeat = [(max(round(i * depth_mul), 1) if i > 1 else i) for i in (num_repeat_backbone + num_repeat_neck)]
-    channels_list = [make_divisible(i * width_mul, 8) for i in (channels_list_backbone + channels_list_neck)]
+    num_repeat = [
+        (max(round(i * depth_mul), 1) if i > 1 else i)
+        for i in (num_repeat_backbone + num_repeat_neck)
+    ]
+    channels_list = [
+        make_divisible(i * width_mul, 8)
+        for i in (channels_list_backbone + channels_list_neck)
+    ]
 
     backbone = EfficientRep(
-        in_channels=ic,
-        channels_list=channels_list,
-        num_repeats=num_repeat
+        in_channels=ic, channels_list=channels_list, num_repeats=num_repeat
     )
 
-    neck = RepPANNeck(
-        channels_list=channels_list,
-        num_repeats=num_repeat
-    )
+    neck = RepPANNeck(channels_list=channels_list, num_repeats=num_repeat)
 
     effidehead_layers = build_effidehead_layer(channels_list, num_anchors, nc)
 
@@ -168,118 +186,100 @@ def build_effidehead_layer(channels_list, num_anchors, nc):
             in_channels=channels_list[6],
             out_channels=channels_list[6],
             kernel_size=1,
-            stride=1
+            stride=1,
         ),
         # cls_conv0
         Conv(
             in_channels=channels_list[6],
             out_channels=channels_list[6],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # reg_conv0
         Conv(
             in_channels=channels_list[6],
             out_channels=channels_list[6],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # cls_pred0
         nn.Conv2d(
-            in_channels=channels_list[6],
-            out_channels=nc * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[6], out_channels=nc * num_anchors, kernel_size=1
         ),
         # reg_pred0
         nn.Conv2d(
-            in_channels=channels_list[6],
-            out_channels=4 * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[6], out_channels=4 * num_anchors, kernel_size=1
         ),
         # obj_pred0
         nn.Conv2d(
-            in_channels=channels_list[6],
-            out_channels=1 * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[6], out_channels=1 * num_anchors, kernel_size=1
         ),
         # stem1
         Conv(
             in_channels=channels_list[8],
             out_channels=channels_list[8],
             kernel_size=1,
-            stride=1
+            stride=1,
         ),
         # cls_conv1
         Conv(
             in_channels=channels_list[8],
             out_channels=channels_list[8],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # reg_conv1
         Conv(
             in_channels=channels_list[8],
             out_channels=channels_list[8],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # cls_pred1
         nn.Conv2d(
-            in_channels=channels_list[8],
-            out_channels=nc * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[8], out_channels=nc * num_anchors, kernel_size=1
         ),
         # reg_pred1
         nn.Conv2d(
-            in_channels=channels_list[8],
-            out_channels=4 * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[8], out_channels=4 * num_anchors, kernel_size=1
         ),
         # obj_pred1
         nn.Conv2d(
-            in_channels=channels_list[8],
-            out_channels=1 * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[8], out_channels=1 * num_anchors, kernel_size=1
         ),
         # stem2
         Conv(
             in_channels=channels_list[10],
             out_channels=channels_list[10],
             kernel_size=1,
-            stride=1
+            stride=1,
         ),
         # cls_conv2
         Conv(
             in_channels=channels_list[10],
             out_channels=channels_list[10],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # reg_conv2
         Conv(
             in_channels=channels_list[10],
             out_channels=channels_list[10],
             kernel_size=3,
-            stride=1
+            stride=1,
         ),
         # cls_pred2
         nn.Conv2d(
-            in_channels=channels_list[10],
-            out_channels=nc * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[10], out_channels=nc * num_anchors, kernel_size=1
         ),
         # reg_pred2
         nn.Conv2d(
-            in_channels=channels_list[10],
-            out_channels=4 * num_anchors,
-            kernel_size=1
+            in_channels=channels_list[10], out_channels=4 * num_anchors, kernel_size=1
         ),
         # obj_pred2
         nn.Conv2d(
-            in_channels=channels_list[10],
-            out_channels=1 * num_anchors,
-            kernel_size=1
-        )
+            in_channels=channels_list[10], out_channels=1 * num_anchors, kernel_size=1
+        ),
     )
 
     return head_layers
